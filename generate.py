026bc612c9b7f76858e1df347b3aa4af36963c70
Vincent Ramdhanie <vramdhanie@gmail.com>
2015-08-30 16:55:37 -0400

    Generate a 2d array of random numbers between 0 and 2 and output to a csv file

--- generate.py
+++ generate.py
@@ -1,4 +1,14 @@
 """Generate a set of n files each containing a sample data set for the
    simulated annealing programs.
 """
- 
+
+import numpy as np
+
+n = 10  # number of data sets to Generate
+M = 25  # number of plots per dataset
+N = 5   # number of attributes per plot
+
+arr = map(lambda row:map(lambda x:x*2, row), np.random.random((M,N)));
+print arr
+
+np.savetxt("foo.csv", arr, delimiter=",", fmt="%1.5f")

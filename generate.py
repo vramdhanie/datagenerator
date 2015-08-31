3d27a893b130a354031799df130a5528fa3d3f61
Vincent Ramdhanie <vramdhanie@gmail.com>
2015-08-31 11:38:31 -0400

    Generate a single file per dataset including assignments and land uses

--- generate.py
+++ generate.py
@@ -8,19 +8,27 @@ import pandas as pd
 n = 10  # number of data sets to Generate
 M = 25  # number of plots per dataset
 N = 5   # number of attributes per plot
+L = 5   # number of land uses
 
-def generateSet(Plots, Attributes):
-    arr = map(lambda row:map(lambda x:x*2, row), np.random.random((Plots, Attributes)))
+def generateSet(M, N):
+    arr = map(lambda row:map(lambda x:x*2, row), np.random.random((M, N)))
     df = pd.DataFrame(arr)
     csv = df.to_csv(path_or_buf=None, sep=',', header=False, index=False)
     return csv
 
 
-f = open('data.txt', 'w')
-for i in range(1, n):
-    data = ("%d\n%d\n%s\n" % (M, N, generateSet(M, N)))
-    f.write(data)
+fileName = "data_"
+
+for i in range(0, n):
+    f = open(("%s%d.txt"%(fileName, i)), 'w')
+    plots = ("%d\n%d\n%s\n" % (M, N, generateSet(M, N)))
+    f.write(plots)
+    landUses = ("%d\n%d\n%s\n" % (L, N, generateSet(L, N)))
+    f.write(landUses)
+
+    for j in range(0, M):
+        f.write(("%d,%d\n"%(j,np.random.randint(L))))
+    f.close()
     M += 5
-f.close()
 
 # np.savetxt("foo.csv", arr, delimiter=",", fmt="%1.5f")
